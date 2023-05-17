8fe4fc83026d51b8aed8b7c64c305795ff7001a2
Khousth <111927511+Khousth@users.noreply.github.com>
2023-05-17 19:41:03 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="app-container">
       <Navbar />
       <Outlet />
-      <p className="fiap">Juliana Angelo Bezerra (RM97508) e Arthur Valente Costa (RM) ----> GIT https://github.com/rm97508/Web____CP06 </p>
+      <p className="fiap">Juliana Angelo Bezerra (RM97508) e Arthur Valente Costa (RM97415) ----> GIT https://github.com/rm97508/Web____CP06 </p>
     </div>
   );
 }
